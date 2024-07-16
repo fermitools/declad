d486e639101bdfa82a069227c8baba01fe649911
Marc Mengel <mengel@fnal.gov>
2024-07-16 15:52:47 -0500

    missing backslash

--- declad/mover.py
+++ declad/mover.py
@@ -270,7 +270,7 @@ class MoverTask(Task, Logged):
                 .replace("$dst_data_path", dest_data_path)   \
                 .replace("$src_data_path", src_data_path)   \
                 .replace("$dst_rel_path", dest_rel_path) \
-                .replace("$adler32_checksum", adler32_checksum)
+                .replace("$adler32_checksum", adler32_checksum) \
                 .replace("$file_size", file_size )
             #self.debug("copy command:", copy_cmd)
 
