1333c55a8c0bceed22677e5adef02df55396f3fc
Alison Peisker <apeisker@fnal.gov>
2025-11-13 10:17:24 -0600

    adding docs to github pages

---
+++ docs/conf.py
@@ -0,0 +1,70 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+print(sys.path)
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'DeclaD'
+copyright = '2023, Igor Mandrichenko'
+author = 'Igor Mandrichenko'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.viewcode',
+    'sphinx_copybutton'
+]
+
+autoclass_content = "both"
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+
+copybutton_prompt_text = r"\$ "
+copybutton_prompt_is_regexp = True
+
+html_theme_options = {
+    "repository_url": "https://github.com/fermitools/declad",
+    "use_repository_button": True,
+    "navigation_depth": 4,  # how many levels of nested headers show in sidebar
+    "collapse_navigation": False
+}
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# moving to fermitools.github.io
+#html_baseurl = 'https://fermitools.github.io/DeclaD/'
